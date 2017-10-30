544a13f21b6ec677e9fb06c0aad3f0b409c17090
LucioCarrera <lcfcnd1@gmail.com>
2017-10-30 09:30:41 -0300

    DriverCommit

---
+++ CkpDriver/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txt_send = New System.Windows.Forms.TextBox()
+        Me.txt_buf = New System.Windows.Forms.TextBox()
+        Me.btn_conectar = New System.Windows.Forms.Button()
+        Me.btn_desconectar = New System.Windows.Forms.Button()
+        Me.btn_send = New System.Windows.Forms.Button()
+        Me.cb_puertos = New System.Windows.Forms.ComboBox()
+        Me.btn_actualizar = New System.Windows.Forms.Button()
+        Me.bw_1 = New System.ComponentModel.BackgroundWorker()
+        Me.sp_1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.txt_console = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txt_send
+        '
+        Me.txt_send.Location = New System.Drawing.Point(12, 12)
+        Me.txt_send.Multiline = True
+        Me.txt_send.Name = "txt_send"
+        Me.txt_send.Size = New System.Drawing.Size(316, 159)
+        Me.txt_send.TabIndex = 1
+        '
+        'txt_buf
+        '
+        Me.txt_buf.Location = New System.Drawing.Point(12, 177)
+        Me.txt_buf.Multiline = True
+        Me.txt_buf.Name = "txt_buf"
+        Me.txt_buf.Size = New System.Drawing.Size(316, 159)
+        Me.txt_buf.TabIndex = 2
+        '
+        'btn_conectar
+        '
+        Me.btn_conectar.Location = New System.Drawing.Point(593, 12)
+        Me.btn_conectar.Name = "btn_conectar"
+        Me.btn_conectar.Size = New System.Drawing.Size(81, 23)
+        Me.btn_conectar.TabIndex = 3
+        Me.btn_conectar.Text = "Conectar"
+        Me.btn_conectar.UseVisualStyleBackColor = True
+        '
+        'btn_desconectar
+        '
+        Me.btn_desconectar.Location = New System.Drawing.Point(593, 42)
+        Me.btn_desconectar.Name = "btn_desconectar"
+        Me.btn_desconectar.Size = New System.Drawing.Size(80, 23)
+        Me.btn_desconectar.TabIndex = 4
+        Me.btn_desconectar.Text = "Desconectar"
+        Me.btn_desconectar.UseVisualStyleBackColor = True
+        '
+        'btn_send
+        '
+        Me.btn_send.Location = New System.Drawing.Point(593, 72)
+        Me.btn_send.Name = "btn_send"
+        Me.btn_send.Size = New System.Drawing.Size(81, 23)
+        Me.btn_send.TabIndex = 5
+        Me.btn_send.Text = "Enviar"
+        Me.btn_send.UseVisualStyleBackColor = True
+        '
+        'cb_puertos
+        '
+        Me.cb_puertos.FormattingEnabled = True
+        Me.cb_puertos.Location = New System.Drawing.Point(334, 12)
+        Me.cb_puertos.Name = "cb_puertos"
+        Me.cb_puertos.Size = New System.Drawing.Size(121, 21)
+        Me.cb_puertos.TabIndex = 6
+        '
+        'btn_actualizar
+        '
+        Me.btn_actualizar.Location = New System.Drawing.Point(461, 10)
+        Me.btn_actualizar.Name = "btn_actualizar"
+        Me.btn_actualizar.Size = New System.Drawing.Size(71, 23)
+        Me.btn_actualizar.TabIndex = 7
+        Me.btn_actualizar.Text = "Actualizar"
+        Me.btn_actualizar.UseVisualStyleBackColor = True
+        '
+        'bw_1
+        '
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1
+        '
+        'txt_console
+        '
+        Me.txt_console.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.txt_console.Location = New System.Drawing.Point(12, 342)
+        Me.txt_console.Multiline = True
+        Me.txt_console.Name = "txt_console"
+        Me.txt_console.ReadOnly = True
+        Me.txt_console.Size = New System.Drawing.Size(662, 163)
+        Me.txt_console.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(686, 517)
+        Me.Controls.Add(Me.txt_console)
+        Me.Controls.Add(Me.btn_actualizar)
+        Me.Controls.Add(Me.cb_puertos)
+        Me.Controls.Add(Me.btn_send)
+        Me.Controls.Add(Me.btn_desconectar)
+        Me.Controls.Add(Me.btn_conectar)
+        Me.Controls.Add(Me.txt_buf)
+        Me.Controls.Add(Me.txt_send)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txt_send As System.Windows.Forms.TextBox
+    Friend WithEvents txt_buf As System.Windows.Forms.TextBox
+    Friend WithEvents btn_conectar As System.Windows.Forms.Button
+    Friend WithEvents btn_desconectar As System.Windows.Forms.Button
+    Friend WithEvents btn_send As System.Windows.Forms.Button
+    Friend WithEvents cb_puertos As System.Windows.Forms.ComboBox
+    Friend WithEvents btn_actualizar As System.Windows.Forms.Button
+    Friend WithEvents bw_1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents sp_1 As System.IO.Ports.SerialPort
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents txt_console As System.Windows.Forms.TextBox
+
+End Class
---
+++ CkpDriver/Form1.vb
@@ -0,0 +1,78 @@
+﻿
+Public Class Form1
+
+    Dim Buffer As String
+    'Actualizar lista de puertos
+    Private Sub btn_actualizar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_actualizar.Click
+        bw_1.RunWorkerAsync()
+
+    End Sub
+    'Form Load
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Control.CheckForIllegalCrossThreadCalls = False
+        btn_actualizar.PerformClick()
+    End Sub
+    'BW 1
+    Private Sub bw_1_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles bw_1.DoWork
+        cb_puertos.Items.Clear()
+        txt_console.Text = txt_console.Text + "Actualizando Puertos..." + vbCrLf
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            Me.cb_puertos.Items.Add(sp)
+        Next
+        txt_console.Text = txt_console.Text + "Puertos Actualizados" + vbCrLf
+    End Sub
+    'Conectar Puerto
+    Private Sub btn_conectar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_conectar.Click
+        If sp_1.IsOpen <> True Then
+            If cb_puertos.SelectedItem = Nothing Then
+                MsgBox("Puerto no seleccionado!", MsgBoxStyle.Critical, "CkpDriver")
+            Else
+                txt_console.Text = txt_console.Text + "Conectando al puerto: " + cb_puertos.SelectedItem.ToString + vbCrLf
+                sp_1.PortName = cb_puertos.SelectedItem.ToString
+                Try
+                    sp_1.Open()
+                    txt_console.Text = txt_console.Text + "Puerto " + cb_puertos.SelectedItem.ToString + " conectado." + vbCrLf
+                Catch ex As Exception
+                    MsgBox("Error: " + ex.ToString, MsgBoxStyle.Critical, "CkpDriver")
+                End Try
+            End If
+        Else
+            MsgBox("El puerto ya esta abierto!", MsgBoxStyle.Information, "CkpDriver")
+        End If
+    End Sub
+    'Cerrar Puerto
+    Private Sub btn_desconectar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_desconectar.Click
+        Try
+            sp_1.Close()
+            txt_console.Text = txt_console.Text + "Puerto " + cb_puertos.SelectedItem.ToString + " desconectado" + vbCrLf
+        Catch ex As Exception
+            MsgBox("Error: " + ex.ToString, MsgBoxStyle.Critical, "CkpDriver")
+        End Try
+    End Sub
+    'Enviar
+    Private Sub btn_send_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_send.Click
+        If sp_1.IsOpen Then
+            Try
+                If txt_send.Text <> "" Then
+                    sp_1.Write(txt_send.Text)
+                    txt_console.Text = txt_console.Text + "Texto Enviado" + vbCrLf
+                Else
+                    MsgBox("No hay texto para enviar!", MsgBoxStyle.Critical, "CkpDriver")
+                End If
+            Catch ex As Exception
+                MsgBox("Error: " + ex.ToString, MsgBoxStyle.Critical, "CkpDriver")
+            End Try
+        Else
+            MsgBox("Puerto cerrado!", MsgBoxStyle.Critical, "CkpDriver")
+        End If
+    End Sub
+    'Timer para recibir data
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If sp_1.IsOpen Then
+            If sp_1.BytesToRead > 0 Then
+                Buffer = sp_1.ReadLine
+                txt_buf.Text = Buffer
+            End If
+        End If
+    End Sub
+End Class
